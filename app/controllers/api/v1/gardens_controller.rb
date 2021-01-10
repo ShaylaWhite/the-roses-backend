91a6d537bdf78ce982c6804cb631cc576fca23ca
ShaylaWhite <shayladwhite1@gmail.com>
2021-01-09 17:42:44 -0800

    methods

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::GradensController < ApplicationController
+
+    def index
+       @gardens = Graden.all
+       render json: @gardens
+     end 
+
+     def create
+        @garden = Garden.new(graden_params)
+         if @garden.
+            render json: @garden
+         else
+            render json {error: `Garden could not be created!`}
+         end 
+    end 
+
+     def show
+        @garden = Garden.find(params[:id])
+        render json: @garden
+    end 
+  
+
+     def destroy
+     end 
+
+   
+     private
+   
+     def garden_params
+        params.require(:garden).permit(:name, :feels, :date)
+     end
+end 
+
 end 
